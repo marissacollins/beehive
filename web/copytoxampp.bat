ccbf3285625689fd3dbe56c9b09ebc9522bf3df1
Hannah Millea <hannahmillea@gmail.com>
2016-02-18 11:26:18 -0500

    initial web code commit

--- web/copytoxampp.bat
+++ web/copytoxampp.bat
@@ -1,14 +1,11 @@
 set root=c:\xampp\htdocs\beehive
-set src=c:\beehive\beehive\web
+set src=C:\Users\hmill\OneDrive\Documents\Beehive\beehive\web
 
-rem cleanout previous version from web htdocs folder
-rem so that any old code is removed
 rmdir /Q /S %root%\
 mkdir %root%\css
 mkdir %root%\images
 mkdir %root%\js
 
-rem copy all the source to the htdocs
 xcopy %src%\css %root%\css /s /e
 xcopy %src%\js %root%\js /s /e
 xcopy %src%\images %root%\images /s /e
