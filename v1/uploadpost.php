7d142dcc783bf664546af2bf67789eb320fd5538
Marissa Collins <marissac44@gmail.com>
2016-04-02 14:09:18 -0400

    created window curl approach for uploading to the backend

--- v1/uploadpost.php
+++ v1/uploadpost.php
@@ -1,38 +1,14 @@
 <?php 
+//with xampp php installed, open a cmdshell and run php uploadpost.php to send a test json to the beehive backend
+// make sure windows path has php eq. c:\xampp\php
 
-/*$json_a = json_decode($string, true);
-
-//$url = "http://localhost/beehive/v1/updateAudio";
-$url = "../v1/updateAudio";
-$content = json_encode($json_a);
-
-$curl = curl_init($url);
-curl_setopt($curl, CURLOPT_HEADER, false);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($curl, CURLOPT_HTTPHEADER,
-        array("Content-type: application/json"));
-curl_setopt($curl, CURLOPT_POST, true);
-curl_setopt($curl, CURLOPT_POSTFIELDS, $content);
-
-$json_response = curl_exec($curl);
-
-$status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-
-if ( $status != 201 ) {
-    die("Error: call to URL $url failed with status $status, response $json_response, curl_error " . curl_error($curl) . ", curl_errno " . curl_errno($curl));
-}
-
-
-curl_close($curl);
-
-$response = json_decode($json_response, true);*/
-
-$string = file_get_contents("c:/beehive/beehive/samplejson/set1/audio.json");
+$string = file_get_contents("c:/beehive/beehive/samplejson/set1/lighthistory.json");
 $json_a = json_decode($string, true);
 
 $url = "http://localhost/v1/uploadStream?hiveid=1";
 $content = json_encode($json_a);
 
+//commandline way to call http services
 $curl = curl_init($url);
 curl_setopt($curl, CURLOPT_HEADER, false);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
@@ -42,6 +18,7 @@ curl_setopt($curl, CURLOPT_HTTPHEADER,
 curl_setopt($curl, CURLOPT_POST, true);
 curl_setopt($curl, CURLOPT_POSTFIELDS, $content);
 
+//execute the request to the server and store reponse in json variable
 $json_response = curl_exec($curl);
 
 $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
